ff9049f324d2d2ea6a92438fa6c2acaa9854afa0
sxianda <harvey.xiandasun@gmail.com>
2016-12-15 14:55:55 -0500

    Create flip-animation.js

---
+++ scripts/flip-animation.js
@@ -0,0 +1,11 @@
+var duration = 1000.0;
+
+function toggleElement(card, targetDegrees) {
+  card.style.transition = "transform " + duration + "ms cubic-bezier(0.3, 0.6, 0.1, 1.0)";
+  card.style.webkitTransition = "-webkit-transform " + duration + "ms cubic-bezier(0.3, 0.6, 0.1, 1.0)";
+  
+  card.style.transform = "rotateY(" + targetDegrees + "deg)";
+  card.style.webkitTransform = "rotateY(" + targetDegrees + "deg)";
+  
+  updateShadows(card, card.style.transition, card.style.transform, duration);
+}
