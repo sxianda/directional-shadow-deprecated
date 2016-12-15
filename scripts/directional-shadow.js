1cc52ca3288271d5c516e8a7e8f606fad3daaaf1
sxianda <harvey.xiandasun@gmail.com>
2016-12-15 14:55:02 -0500

    Update directional-shadow.js

--- scripts/directional-shadow.js
+++ scripts/directional-shadow.js
@@ -1,2 +1,75 @@
 function initializeDirectionalShadow(clip) {
+  var cards = clip.querySelectorAll('div.directional-shadow');
+  
+  // Insert shadow divs for each directional-shadow element.
+  for (var i = 0; i < cards.length; i++) {
+    var elem = cards[i];
+    var elemClassStr = elem.className;
+    elemClassStr = elemClassStr.replace(/\bdirectional-shadow\b/,'');
+    
+    var shadowX = elem.getAttribute('shadow-x');
+    var shadowY = elem.getAttribute('shadow-y');
+    var shadowDepth = elem.getAttribute('shadow-depth');
+    
+    // Elem's parent is perspective, perspective's parent is shadow-collection;
+    var parent = elem.parentElement.parentElement;
+    
+    // Create shadow elements.
+    var shadowPerspective = document.createElement("div");
+    shadowPerspective.className = "perspective-shadow";
+    shadowPerspective.style.webkitPerspectiveOrigin = shadowX + "% " + shadowY + "%";
+    shadowPerspective.style.webkitPerspective = shadowDepth;
+    
+    var shadowCard = document.createElement("div");
+    shadowCard.className = "shadow-card" + elemClassStr;
+    
+    var shadow = document.createElement("div");
+    shadow.className = "shadow";
+    
+    parent.insertBefore(shadowPerspective, parent.childNodes[0]);
+    shadowPerspective.appendChild(shadowCard);
+    shadowCard.appendChild(shadow);
+    
+    // Create shadow gradient elements.
+    var shadowGradientPerspective = document.createElement("div");
+    shadowGradientPerspective.className = "perspective-shadow";
+    shadowGradientPerspective.style.webkitPerspectiveOrigin = shadowX + "% " + shadowY + "%";
+    shadowGradientPerspective.style.webkitPerspective = shadowDepth;
+    
+    var shadowCardGradient = document.createElement("div");
+    shadowCardGradient.className="shadow-card-gradient" + elemClassStr;
+    
+    var shadowGradient = document.createElement("div");
+    shadowGradient.className="shadow";
+    
+    parent.insertBefore(shadowGradientPerspective, parent.childNodes[0]);
+    shadowGradientPerspective.appendChild(shadowCardGradient);
+    shadowCardGradient.appendChild(shadowGradient);
+    
+    // Add event listeners
+    
+  }
+}
+
+function updateShadows(card, transition, transform, duration) {
+  var collection = card.parentElement.parentElement;
+  var shadowCard = collection.querySelectorAll('div.shadow-card')[0];
+  var shadowCardGradient = collection.querySelectorAll('div.shadow-card-gradient')[0];
+  
+  
+  shadowCard.style.transformOrigin = "50% 0%";
+  shadowCardGradient.style.transformOrigin = "50% 0%";
+  
+  shadowCard.style.webkitTransition = transition;
+  shadowCardGradient.style.webkitTransition = transition;
+  
+  shadowCard.style.webkitTransform = transform;
+  shadowCardGradient.style.webkitTransform = transform;
+  
+  shadowCard.style.webkitAnimation = "";
+  shadowCardGradient.style.webkitAnimation = "";
+  document.body.offsetTop;
+  
+  shadowCard.style.webkitAnimation = "smooth-step " + duration + "ms";
+  shadowCardGradient.style.webkitAnimation = "smooth-step-gradient " + duration + "ms";
 }
