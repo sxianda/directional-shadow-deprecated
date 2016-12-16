5b1ad9880a59314e75b009ba7c1a84f4052a8ed3
sxianda <harvey.xiandasun@gmail.com>
2016-12-15 23:24:30 -0500

    Update flip-animation.js

--- scripts/flip-animation.js
+++ scripts/flip-animation.js
@@ -1,11 +1,11 @@
 var duration = 1000.0;
 
-function toggleElement(card, targetDegrees) {
+function toggleElement(card, distance, targetDegrees) {
   card.style.transition = "transform " + duration + "ms cubic-bezier(0.3, 0.6, 0.1, 1.0)";
   card.style.webkitTransition = "-webkit-transform " + duration + "ms cubic-bezier(0.3, 0.6, 0.1, 1.0)";
   
-  card.style.transform = "translateZ(100px) rotateY(" + targetDegrees + "deg)";
-  card.style.webkitTransform = "translateZ(100px) rotateY(" + targetDegrees + "deg)";
+  card.style.transform = "translateZ(" + distance + "px) rotateY(" + targetDegrees + "deg)";
+  card.style.webkitTransform = "translateZ(" + distance + "px) rotateY(" + targetDegrees + "deg)";
   
   updateShadows(card, card.style.transition, card.style.transform);
 }
