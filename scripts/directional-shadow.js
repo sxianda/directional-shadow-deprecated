93946c4238593962182256a886f50cf7e5a07df8
sxianda <harvey.xiandasun@gmail.com>
2017-02-10 10:35:06 -0500

    Update directional-shadow.js

--- scripts/directional-shadow.js
+++ scripts/directional-shadow.js
@@ -66,12 +66,13 @@ function initializeDirectionalShadow(clip) {
     shadowGradientBack.className = "shadow-gradient-back";
     shadowGradientBack.style.width = (elem.offsetWidth - 10) + "px";
     shadowGradientBack.style.height = (elem.offsetHeight - 10) + "px";
-    shadowGradientBack.style.transform = rotateY(180deg);
-    shadowGradientBack.style.webkitTransform = rotateY(180deg);
+    shadowGradientBack.style.transform = "rotateY(180deg)";
+    shadowGradientBack.style.webkitTransform = "rotateY(180deg)";
 	  
     parent.insertBefore(shadowGradientPerspective, parent.childNodes[0]);
     shadowGradientPerspective.appendChild(shadowCardGradient);
     shadowCardGradient.appendChild(shadowGradient);
+    shadowCardGradient.appendChild(shadowGradientBack);
     
     // Add DOMMutObserver
     observer.observe(elem, config);
