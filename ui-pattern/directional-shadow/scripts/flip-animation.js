8221e98d2d0ffd33c06512b0899748110ab6cdf2
sxianda <harvey.xiandasun@gmail.com>
2017-02-28 16:43:38 -0500

    Create flip-animation.js

---
+++ ui-pattern/directional-shadow/scripts/flip-animation.js
@@ -0,0 +1,55 @@
+var duration = 600.0;
+var angleX = 0;
+var angleY = 0;
+var angleZ = 0;
+
+function toggleElement(card, distance, targetDegrees) {
+  cardRotateY(card, targetDegrees);
+}
+
+function cardRotateX(card, deg) {
+  card.style.transition = "transform " + duration + "ms cubic-bezier(0.3, 0.6, 0.1, 1.0)";
+  var distance = window.getComputedStyle(card).getPropertyValue("transform-origin").split(" ")[0];
+  card.style.transform = "translateZ(" + distance + ") rotateX(" + deg + "deg)";
+}
+  
+function cardRotateY(card, deg) {
+  card.style.transition = "transform " + duration + "ms cubic-bezier(0.3, 0.6, 0.1, 1.0)";
+  var distance = window.getComputedStyle(card).getPropertyValue("transform-origin").split(" ")[0];
+  card.style.transform = "translateZ(" + distance + ") rotateY(" + deg + "deg)";
+}
+
+function cardRotateZ(card, deg) {
+  card.style.transition = "transform " + duration + "ms cubic-bezier(0.3, 0.6, 0.1, 1.0)";
+  var distance = window.getComputedStyle(card).getPropertyValue("transform-origin").split(" ")[0];
+  card.style.transform = "translateZ(" + distance + ") rotateZ(" + deg + "deg)";
+}
+
+function flipX(distance) {
+  var card = document.getElementById("square-animation");
+  angleX += 45;
+  card.style.transition = "transform " + duration + "ms cubic-bezier(0.3, 0.6, 0.1, 1.0)";
+
+  var transform = "translateZ(" + distance + "px) rotateX(" + angleX + "deg) rotateY(" + angleY
+      + "deg) rotateZ(" + angleZ + "deg)";
+
+  card.style.transform = transform;
+}
+
+function flipY(distance) {
+  var card = document.getElementById("square-animation");
+  angleY += 45;
+  card.style.transition = "transform " + duration + "ms cubic-bezier(0.3, 0.6, 0.1, 1.0)";
+
+  card.style.transform = "translateZ(" + distance + "px) rotateX(" + angleX + "deg) rotateY(" + angleY
+      + "deg) rotateZ(" + angleZ + "deg)";
+}
+
+function flipZ(distance) {
+  var card = document.getElementById("square-animation");
+  angleZ += 45;
+  card.style.transition = "transform " + duration + "ms cubic-bezier(0.3, 0.6, 0.1, 1.0)";
+
+  card.style.transform = "translateZ(" + distance + "px) rotateX(" + angleX + "deg) rotateY(" + angleY
+      + "deg) rotateZ(" + angleZ + "deg)";
+}
